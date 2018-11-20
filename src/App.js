194e36849547afa958644194ea9932b976eb7dad
pedro <pedroluismontesinosrivera@gmail.com>
2018-11-20 00:51:52 -0500

    using geolocation api

--- src/App.js
+++ src/App.js
@@ -2,7 +2,18 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      latitude: null
+    }
+  }
+
   render() {
+    window.navigator.geolocation.getCurrentPosition(
+      position => console.log(position),
+      error => console.log(error)
+    );
     return (
       <div className="App">
         <h1>Homepage</h1>
---
+++ src/components/SeasonDisplay.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class SeasonDisplay extends Component {
+    render() {
+        return (
+            <div className="App">
+                <h1>SeasonDisplay</h1>
+            </div>
+        );
+    }
+}
+
+export default SeasonDisplay;
