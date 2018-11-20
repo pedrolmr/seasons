a7fc9b57967270f53fe32a4ba189eb5a57f2d082
pedro <pedroluismontesinosrivera@gmail.com>
2018-11-20 05:18:46 -0500

    implemented logic to know if it's winter ot summer

--- src/App.js
+++ src/App.js
@@ -1,30 +1,22 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import SeasonDisplay from './components/SeasonDisplay';
+
 class App extends Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      latitude: null,
-      errorMessage: ''
-    }
-  }
+    state = {latitude: null, errorMessage: ''}
 
   componentDidMount(){
     window.navigator.geolocation.getCurrentPosition(
-      position => {
-        this.setState({latitude: position.coords.latitude})
-      },
-      error => {
-        this.setState({errorMessage: error.message})
-      }
+      position => this.setState({latitude: position.coords.latitude}),
+      error => this.setState({errorMessage: error.message})
     );
   }
 
   render() {
     return (
       <div className="App">
-      {!this.state.latitude ? this.state.errorMessage : this.state.latitude}
+      {!this.state.latitude ? this.state.errorMessage : <SeasonDisplay latitude={this.state.latitude}/>}
       </div>
     );
   }
--- src/components/SeasonDisplay.js
+++ src/components/SeasonDisplay.js
@@ -1,14 +1,21 @@
-import React, { Component } from 'react';
-import './App.css';
-
-class SeasonDisplay extends Component {
-    render() {
-        return (
-            <div className="App">
-                <h1>SeasonDisplay</h1>
-            </div>
-        );
+import React from 'react';
+const getSeason = (latitude, month) => {
+    if(month > 2 && month < 9){
+        return latitude > 0 ? 'summer' : 'winter';
+    }else{
+        return latitude > 0 ? 'winter' : 'summer';
     }
+}
+
+const SeasonDisplay = (props) => {
+    const season = getSeason(props.latitude, new Date().getMonth());
+
+    return (
+        <div className='season-display'>
+            <h1>{season === 'winter' ? 'it\'s winter!!' : 'it\'s summer!!'}</h1>
+        </div>
+    );
+    
 }
 
 export default SeasonDisplay;
